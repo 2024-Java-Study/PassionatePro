f0b8ee1a90908d4da0c95c86f16c1399bdafbed1
NamJihyun99 <namjihyun9936@gmail.com>
2024-01-16 18:39:10 +0900

    feat:> 공통 응답 메시지/#1

---
+++ pro/src/main/java/com/example/pro/common/response/BasicResponse.java
@@ -0,0 +1,12 @@
+package com.example.pro.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BasicResponse<T> {
+
+    private boolean success;
+    private T response;
+}
