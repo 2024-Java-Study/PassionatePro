efbb8406d6a8278c84581a6bf5b8234925b16e58
ajeong <ajung7038@naver.com>
2024-01-25 20:04:13 +0900

    Feat: validateCreateBoard 테스트케이스 추가 #6

--- pro/src/test/java/com/example/pro/board/BoardRepositoryCreateTest.java
+++ pro/src/test/java/com/example/pro/board/BoardRepositoryCreateTest.java
@@ -1,7 +1,7 @@
 package com.example.pro.board;
 
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,22 +9,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class) // junit과 스프링을 엮어서 실행
 @SpringBootTest // 스프링 컨테이너 안에서 테스트를 돌린다
 @Transactional // 롤백
-class BoardRepositoryTest {
+class BoardRepositoryCreateTest {
 
     @Autowired BoardRepository boardRepository;
+//    Board board;
+
+    @BeforeEach
+    public void setUp() {
+//        board = new Board("제목", "내용");
+    }
 
     @Test
     @DisplayName("게시글 생성")
     public void CreateBoard() throws Exception {
         // given
         Board board = new Board("제목", "내용");
+
         // when
-        Board createBoard = boardRepository.save(board);
+        Board createdBoard = boardRepository.save(board);
 
         // then
-        Assertions.assertEquals(board, createBoard);
+        assertThat(board).isEqualTo(createdBoard);
+    }
+    
+    @Test
+    @DisplayName("Board 생성 시 null 값이 없어야 한다")
+    public void validateCreateBoard() throws Exception {
+        // given
+        Board board = new Board("제목", "내용");
+
+        // then
+//        assertThat(board.getMember()).isNotNull();
+        assertThat(board.getTitle()).isNotNull();
+        assertThat(board.getContent()).isNotNull();
     }
 }
