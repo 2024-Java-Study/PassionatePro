c53f41901566dbd75a3478c9ea20abc094be9659
NamJihyun99 <namjihyun9936@gmail.com>
2024-02-27 01:39:32 +0900

    fix:> domain classes /#18

--- pro/src/main/java/com/example/pro/comment/domain/Comment.java
+++ pro/src/main/java/com/example/pro/comment/domain/Comment.java
@@ -1,50 +1,47 @@
 package com.example.pro.comment.domain;
 
+import com.example.pro.auth.domain.Member;
 import com.example.pro.board.domain.Board;
-import com.example.pro.domain.Reply;
+import com.example.pro.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Comment {
+public class Comment extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id")
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
     @OneToMany(mappedBy = "comment")
-    private List<Reply> reply = new ArrayList<>();
+    private final List<Reply> replies = new ArrayList<>();
 
     @Column(nullable = false)
     private String content;
-    private LocalDateTime created_at;
 
     /**
      * 생성 메서드
      */
     @Builder
-//    public Comment(Member member, Board board, String content) {
-    public Comment(Board board, String content) {
-//        this.member = member;
+    public Comment(Member member, Board board, String content) {
+        this.member = member;
         this.board = board;
         this.content = content;
-        this.created_at = LocalDateTime.now();
     }
 }
--- pro/src/main/java/com/example/pro/comment/domain/Reply.java
+++ pro/src/main/java/com/example/pro/comment/domain/Reply.java
@@ -1,43 +1,37 @@
-package com.example.pro.domain;
+package com.example.pro.comment.domain;
 
-import com.example.pro.comment.domain.Comment;
+import com.example.pro.auth.domain.Member;
+import com.example.pro.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Reply {
+public class Reply extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reply_id")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id")
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id", nullable = false)
     private Comment comment;
 
     private String content;
-    private LocalDateTime created_at;
 
     /**
      * 생성 메서드
      */
-//    public Reply(Member member, Board board, String content) {
-    public Reply(Comment comment, String content) {
-//        this.member = member;
-        // board?
+    public Reply(Member member, Comment comment, String content) {
+        this.member = member;
         this.comment = comment;
         this.content = content;
-        this.created_at = LocalDateTime.now();
     }
 }
