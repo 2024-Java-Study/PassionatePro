1fce0f9c2e48d5d2b23ccc6f606a99005645ffa7
NamJihyun99 <namjihyun9936@gmail.com>
2024-02-27 01:52:14 +0900

    feat:> repository interfaces/#18

---
+++ pro/src/main/java/com/example/pro/comment/repository/ReplyRepository.java
@@ -0,0 +1,12 @@
+package com.example.pro.comment.repository;
+
+import com.example.pro.comment.domain.Comment;
+import com.example.pro.comment.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findAllByComment(Comment comment);
+}
