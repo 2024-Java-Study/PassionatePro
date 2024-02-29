2a6bf959352a689d2c0e0fafa9d9b61d50f9c54e
NamJihyun99 <namjihyun9936@gmail.com>
2024-02-29 16:57:54 +0900

    test:> reply controller test/#18

--- pro/src/test/java/com/example/pro/comment/controller/ReplyControllerTest.java
+++ pro/src/test/java/com/example/pro/comment/controller/ReplyControllerTest.java
@@ -4,11 +4,12 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.example.pro.auth.domain.Member;
 import com.example.pro.auth.service.AuthService;
 import com.example.pro.board.domain.Board;
-import com.example.pro.board.exception.BoardErrorCode;
-import com.example.pro.board.exception.BoardException;
 import com.example.pro.comment.domain.Comment;
-import com.example.pro.comment.dto.CommentSaveRequestDto;
-import com.example.pro.comment.service.CommentService;
+import com.example.pro.comment.domain.Reply;
+import com.example.pro.comment.dto.ReplySaveRequestDto;
+import com.example.pro.comment.exception.CommentErrorCode;
+import com.example.pro.comment.exception.CommentException;
+import com.example.pro.comment.service.ReplyService;
 import com.example.pro.docs.ControllerTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -37,20 +38,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
-class CommentControllerTest extends ControllerTest {
-
-    @Mock
-    CommentService commentService;
+class ReplyControllerTest extends ControllerTest {
     @Mock
     AuthService authService;
-    CommentSaveRequestDto request;
-    Comment comment;
-    Board board;
+    @Mock
+    ReplyService replyService;
     Member member;
+    Board board;
+    Comment comment;
+    Reply reply;
+    ReplySaveRequestDto saveRequest;
 
     @BeforeEach
     void init() {
-        request = new CommentSaveRequestDto(1L, "댓글 내용 빈칸 아님");
+        member = Member.builder()
+                .username("comment-writer")
+                .password("password1234")
+                .nickname("nickname")
+                .email("helloworld@gmail.com")
+                .build();
 
         board = Board.builder()
                 .id(1L)
@@ -59,30 +65,33 @@ class CommentControllerTest extends ControllerTest {
                 .content("게시글 내용")
                 .build();
 
-        member = Member.builder()
-                .username("comment-writer")
-                .password("password1234")
-                .nickname("nickname")
-                .email("helloworld@gmail.com")
-                .build();
-
         comment = Comment.builder()
                 .id(1L)
                 .board(board)
                 .member(member)
                 .content("댓글 내용 빈칸 아님")
                 .build();
+
+        reply = Reply.builder()
+                .id(1L)
+                .member(member)
+                .comment(comment)
+                .content("대딧글 내용")
+                .build();
+
+        saveRequest = new ReplySaveRequestDto(1L, "대딧글 내용");
     }
 
     @Test
-    @DisplayName("[성공] 댓글 저장 API")
-    void saveComment() throws Exception {
+    @DisplayName("[성공] 대댓글 작성")
+    void saveReply() throws Exception {
+
         when(authService.loadUser()).thenReturn(member);
-        when(commentService.saveComment(any(), any())).thenReturn(comment);
+        when(replyService.saveReply(any(), any())).thenReturn(reply);
 
-        String body = objectMapper.writeValueAsString(request);
+        String body = objectMapper.writeValueAsString(saveRequest);
 
-        ResultActions perform = mockMvc.perform(post("/comments")
+        ResultActions perform = mockMvc.perform(post("/replies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(body)
                 .locale(Locale.KOREAN)
@@ -93,13 +102,13 @@ class CommentControllerTest extends ControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.response").value("댓글이 성공적으로 등록되었습니다. Comment Id: 1"));
+                .andExpect(jsonPath("$.response").value("답글이 성공적으로 등록되었습니다. Reply Id: 1"));
 
-        perform.andDo(document("comment save-success",
+        perform.andDo(document("reply save-success",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 resource(ResourceSnippetParameters.builder()
-                        .tag("API-Comment")
+                        .tag("API-Reply")
                         .responseFields(
                                 fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
                                 fieldWithPath("response").type(JsonFieldType.STRING).description("응답 메시지")
@@ -108,12 +117,13 @@ class CommentControllerTest extends ControllerTest {
     }
 
     @Test
-    @DisplayName("[실패] 댓글 저장-유효성 검사 실패")
-    void saveCommentValidation() throws Exception {
-        request = new CommentSaveRequestDto(null, "  ");
-        String body = objectMapper.writeValueAsString(request);
+    @DisplayName("[실패] 대댓글 작성-유효성 검사 실패")
+    void saveReplyValidationFail() throws Exception {
+        saveRequest = new ReplySaveRequestDto(null, " ");
 
-        ResultActions perform = mockMvc.perform(post("/comments")
+        String body = objectMapper.writeValueAsString(saveRequest);
+
+        ResultActions perform = mockMvc.perform(post("/replies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .locale(Locale.KOREAN)
                 .content(body)
@@ -126,33 +136,34 @@ class CommentControllerTest extends ControllerTest {
                 .andExpect(jsonPath("$.success").value(false))
                 .andExpect(jsonPath("$.response.errorCode").value("BAD_INPUT"))
                 .andExpect(jsonPath("$.response.errorMessage").value("입력이 올바르지 않습니다."))
-                .andExpect(jsonPath("$.response.errors.boardId").value("널이어서는 안됩니다"))
+                .andExpect(jsonPath("$.response.errors.commentId").value("널이어서는 안됩니다"))
                 .andExpect(jsonPath("$.response.errors.content").value("공백일 수 없습니다"));
 
-        perform.andDo(document("comment save-validation failed",
+        perform.andDo(document("reply save-validation failed",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 resource(ResourceSnippetParameters.builder()
-                        .tag("API-Comment")
+                        .tag("API-Reply")
                         .responseFields(
                                 fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
                                 fieldWithPath("response.errorCode").type(JsonFieldType.STRING).description("예외 코드"),
                                 fieldWithPath("response.errorMessage").type(JsonFieldType.STRING).description("예외 메시지"),
-                                fieldWithPath("response.errors.boardId").type(JsonFieldType.STRING).description("게시물 id null 검사"),
-                                fieldWithPath("response.errors.content").type(JsonFieldType.STRING).description("댓글 내용 공백 검사")
+                                fieldWithPath("response.errors.commentId").type(JsonFieldType.STRING).description("댓글 id null 검사"),
+                                fieldWithPath("response.errors.content").type(JsonFieldType.STRING).description("답글 내용 공백 검사")
                         ).build())
         ));
     }
 
     @Test
-    @DisplayName("[실패] 댓글 저장-존재하지 않는 board id")
-    void boardIdNotFound() throws Exception {
+    @DisplayName("[실패] 대댓글 작성-잘못된 댓글 id")
+    void saveReplyCommentNotFound() throws Exception {
         when(authService.loadUser()).thenReturn(member);
-        when(commentService.saveComment(any(), any()))
-                .thenThrow(new BoardException(BoardErrorCode.BOARD_NOT_FOUND));
-        String body = objectMapper.writeValueAsString(request);
+        when(replyService.saveReply(any(), any()))
+                .thenThrow(new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
 
-        ResultActions perform = mockMvc.perform(post("/comments")
+        String body = objectMapper.writeValueAsString(saveRequest);
+
+        ResultActions perform = mockMvc.perform(post("/replies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(body)
                 .characterEncoding(StandardCharsets.UTF_8)
@@ -160,17 +171,17 @@ class CommentControllerTest extends ControllerTest {
 
         perform.andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(BoardException.class))
+                .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(CommentException.class))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(false))
-                .andExpect(jsonPath("$.response.errorCode").value("BOARD_NOT_FOUND"))
-                .andExpect(jsonPath("$.response.errorMessage").value("게시물을 찾을 수 없습니다."));
+                .andExpect(jsonPath("$.response.errorCode").value("COMMENT_NOT_FOUND"))
+                .andExpect(jsonPath("$.response.errorMessage").value("해당 댓글을 찾을 수 없습니다."));
 
-        perform.andDo(document("comment save-board not found",
+        perform.andDo(document("reply save-comment not found",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 resource(ResourceSnippetParameters.builder()
-                        .tag("API-Comment")
+                        .tag("API-Reply")
                         .responseFields(
                                 fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
                                 fieldWithPath("response.errorCode").type(JsonFieldType.STRING).description("예외 코드"),
@@ -182,6 +193,6 @@ class CommentControllerTest extends ControllerTest {
 
     @Override
     protected Object injectController() {
-        return new CommentController(authService, commentService);
+        return new ReplyController(replyService, authService);
     }
 }
