7db3456e519574bee3b3d886a8770a4d53d9445b
NamJihyun99 <namjihyun9936@gmail.com>
2024-03-08 16:32:27 +0900

    del:>delete unnecessary imports/#18

--- pro/src/main/java/com/example/pro/files/FileUploader.java
+++ pro/src/main/java/com/example/pro/files/FileUploader.java
@@ -1,12 +1,8 @@
 package com.example.pro.files;
 
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
 
 public interface FileUploader {
--- pro/src/main/java/com/example/pro/files/TestFileUploader.java
+++ pro/src/main/java/com/example/pro/files/TestFileUploader.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
