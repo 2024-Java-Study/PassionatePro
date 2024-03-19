34d5d8fb289ae80e67099c0a03f84448b741cdc4
NamJihyun99 <namjihyun9936@gmail.com>
2024-03-19 16:15:28 +0900

    test:> comment delete api test/#18

--- pro/src/test/java/com/example/pro/comment/controller/CommentControllerTest.java
+++ pro/src/test/java/com/example/pro/comment/controller/CommentControllerTest.java
@@ -30,9 +30,9 @@ import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.docume
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -323,6 +323,105 @@ class CommentControllerTest extends ControllerTest {
                                 fieldWithPath("response.errorCode").type(JsonFieldType.STRING).description("예외 코드"),
                                 fieldWithPath("response.errorMessage").type(JsonFieldType.STRING).description("예외 메시지"),
                                 fieldWithPath("response.errors.content").type(JsonFieldType.STRING).description("댓글 내용 공백 검사")
+                        ).build())
+        ));
+    }
+
+    @Test
+    @DisplayName("[성공] 댓글 삭제")
+    void deleteComment() throws Exception {
+        when(authService.loadUser()).thenReturn(member);
+
+        ResultActions perform = mockMvc.perform(delete("/comments/{commentId}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .locale(Locale.KOREAN)
+                .characterEncoding(StandardCharsets.UTF_8)
+        );
+
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.response").value("댓글이 성공적으로 삭제되었습니다."));
+
+        perform.andDo(document("comment delete-success",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(ResourceSnippetParameters.builder()
+                        .tag("API-Comment")
+                        .responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
+                                fieldWithPath("response").type(JsonFieldType.STRING).description("응답 메시지")
+                        ).build())
+        ));
+    }
+
+    @Test
+    @DisplayName("[실패] 댓글 삭제-존재하지 않는 댓글id")
+    void deleteCommentNotFound() throws Exception {
+        when(authService.loadUser()).thenReturn(member);
+        doThrow(new CommentException(CommentErrorCode.COMMENT_NOT_FOUND))
+                .when(commentService).deleteComment(any(), any());
+
+        ResultActions perform = mockMvc.perform(delete("/comments/{commentId}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .locale(Locale.KOREAN)
+                .characterEncoding(StandardCharsets.UTF_8)
+        );
+
+        perform.andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(CommentException.class))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.response.errorCode").value("COMMENT_NOT_FOUND"))
+                .andExpect(jsonPath("$.response.errorMessage").value("해당 댓글을 찾을 수 없습니다."));
+
+        perform.andDo(document("comment delete-comment not found",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(ResourceSnippetParameters.builder()
+                        .tag("API-Comment")
+                        .responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
+                                fieldWithPath("response.errorCode").type(JsonFieldType.STRING).description("예외 코드"),
+                                fieldWithPath("response.errorMessage").type(JsonFieldType.STRING).description("예외 메시지"),
+                                fieldWithPath("response.errors").type(JsonFieldType.OBJECT).description("필드 유효성 검사 내용")
+                        ).build())
+        ));
+    }
+
+    @Test
+    @DisplayName("[실패] 댓글 삭제-삭제 권한 없음")
+    void deleteCommentNotPermitted() throws Exception {
+        when(authService.loadUser()).thenReturn(member);
+        doThrow(new CommentException(CommentErrorCode.COMMENT_ACCESS_NOT_PERMITTED))
+                .when(commentService).deleteComment(any(), any());
+
+        ResultActions perform = mockMvc.perform(delete("/comments/{commentId}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .locale(Locale.KOREAN)
+                .characterEncoding(StandardCharsets.UTF_8)
+        );
+
+        perform.andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(CommentException.class))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.response.errorCode").value("COMMENT_ACCESS_NOT_PERMITTED"))
+                .andExpect(jsonPath("$.response.errorMessage").value("해당 댓글에 접근할 권한이 없습니다."));
+
+        perform.andDo(document("comment delete-not permitted",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(ResourceSnippetParameters.builder()
+                        .tag("API-Comment")
+                        .responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 정상 여부"),
+                                fieldWithPath("response.errorCode").type(JsonFieldType.STRING).description("예외 코드"),
+                                fieldWithPath("response.errorMessage").type(JsonFieldType.STRING).description("예외 메시지"),
+                                fieldWithPath("response.errors").type(JsonFieldType.OBJECT).description("필드 유효성 검사 내용")
                         ).build())
         ));
     }
