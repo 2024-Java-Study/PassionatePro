28586d5a8bf99ce001e5d4262e997d6705a4894e
NamJihyun99 <namjihyun9936@gmail.com>
2024-04-30 16:36:00 +0900

    add:>add auth exception log/#30

--- pro/src/main/java/com/example/pro/auth/exception/SessionAuthenticationEntryPoint.java
+++ pro/src/main/java/com/example/pro/auth/exception/SessionAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import com.example.pro.common.response.ResponseUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class SessionAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        log.error("Auth Exception({})={}", AuthErrorCode.UNAUTHORIZED_USER.toString(), AuthErrorCode.UNAUTHORIZED_USER.getMessage());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setCharacterEncoding("utf-8");
