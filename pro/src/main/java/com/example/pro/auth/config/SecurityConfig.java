aba2c392a733ab01ea4708b6d157dee116afcc35
NamJihyun99 <namjihyun9936@gmail.com>
2024-07-03 19:00:37 +0900

    fix:> addAllowHeader set-cookie/#30

--- pro/src/main/java/com/example/pro/auth/config/SecurityConfig.java
+++ pro/src/main/java/com/example/pro/auth/config/SecurityConfig.java
@@ -121,6 +121,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.addAllowedHeader("Set-Cookie");
         configuration.setMaxAge(7200L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
