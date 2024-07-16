0446ae854e08162c73fe8dd627e4d84c36fecad0
NamJihyun99 <namjihyun9936@gmail.com>
2024-07-16 09:53:06 +0900

    feat:> quit and signout /#118

--- pro/src/main/java/com/example/pro/comment/repository/CommentRepository.java
+++ pro/src/main/java/com/example/pro/comment/repository/CommentRepository.java
@@ -2,13 +2,15 @@ package com.example.pro.comment.repository;
 
 import com.example.pro.board.domain.Board;
 import com.example.pro.comment.domain.Comment;
-import com.example.pro.comment.domain.WriterInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByBoard(Board board);
-    List<Comment> findAllByWriter(WriterInfo writer);
+    @Query("select comment from Comment comment where comment.writer.username = :writerName")
+    List<Comment> findAllByWriter(@Param("writerName") String writerName);
 }
--- pro/src/main/java/com/example/pro/comment/repository/ReplyRepository.java
+++ pro/src/main/java/com/example/pro/comment/repository/ReplyRepository.java
@@ -4,11 +4,14 @@ import com.example.pro.comment.domain.Comment;
 import com.example.pro.comment.domain.Reply;
 import com.example.pro.comment.domain.WriterInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     List<Reply> findAllByComment(Comment comment);
-    List<Reply> findAllByWriter(WriterInfo writer);
+    @Query("select reply from Reply reply where reply.writer.username = :writerName")
+    List<Reply> findAllByWriter(@Param("writerName") String writerName);
 }
